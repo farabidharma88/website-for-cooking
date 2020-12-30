29ea0e04ed1aa38d82f7571874b56986e1d30cfe
FARABI DHARMA <74946238+farabidharma88@users.noreply.github.com>
2020-12-30 17:26:13 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,129 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+const algoliasearch = require("algoliasearch");
+
+const APP_ID = "VC8WCMFUPU";
+const ADMIN_KEY = "992f28c233e01c9e8c8c0a7c3b5d2056";
+
+const client = algoliasearch(APP_ID, ADMIN_KEY);
+const index = client.initIndex("resep");
+
+app.post("/regis", async (req, res) => {
+  const { uid, displayName } = req.body;
+  if (!uid || !displayName) {
+    console.log("params ga lengkap");
+    return res.status(400).json({ pesan: "Params ga lengkap" });
+  }
+  return await db
+    .collection("Users")
+    .doc(uid)
+    .set({
+      displayName: displayName,
+      ListResep: [],
+      listCookLater: [],
+      uid: uid,
+      bio: "",
+    })
+    .then(() => {
+      return res.json({ pesan: "berhasil membuat data" });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ pesan: "gagal" });
+    });
+});
+
+app.get("/AddToAlgolia", async (req, res) => {
+  return db
+    .collection("resep")
+    .get()
+    .then((result) => {
+      let arr = [];
+      result.docs.forEach((doc) => {
+        const data = doc.data();
+        const objectID = doc.id;
+        arr.push({
+          bahan: data.bahan,
+          cookLaterCount: data.cookLaterCount,
+          jmlBahan: data.jmlBahan,
+          judul: data.judul,
+          pembuatResep: data.pembuatResep,
+          thumbnail: data.thumbnail,
+          timestamp: data.timestamp,
+          objectID,
+        });
+      });
+      return index
+        .saveObjects(arr)
+        .then(({ objectIDs }) => {
+          console.log(objectIDs);
+          return res.json(objectIDs);
+        })
+        .catch((err) => {
+          return res.status(500).json(err);
+        });
+    });
+});
+app.post("/coba", (req, res) => {
+  return res.json({ pesan: "ini post" });
+});
+exports.api = functions.region("asia-southeast2").https.onRequest(app);
+
+exports.addToIndex = functions
+  .region("asia-southeast2")
+  .firestore.document("resep/{resepId}")
+  .onCreate((snapshot) => {
+    const data = snapshot.data();
+    const objectID = snapshot.id;
+    return index
+      .saveObject({
+        bahan: data.bahan,
+        cookLaterCount: data.cookLaterCount,
+        jmlBahan: data.jmlBahan,
+        judul: data.judul,
+        pembuatResep: data.pembuatResep,
+        thumbnail: data.thumbnail,
+        timestamp: data.timestamp,
+        objectID,
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+exports.updateIndex = functions
+  .region("asia-southeast2")
+  .firestore.document("resep/{resepId}")
+  .onUpdate((change) => {
+    const newData = change.after.data();
+    const objectID = change.after.id;
+    return index
+      .saveObject({
+        bahan: newData.bahan,
+        cookLaterCount: newData.cookLaterCount,
+        jmlBahan: newData.jmlBahan,
+        judul: newData.judul,
+        pembuatResep: newData.pembuatResep,
+        thumbnail: newData.thumbnail,
+        timestamp: newData.timestamp,
+        objectID,
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+exports.deleteFromIndex = functions
+  .region("asia-southeast2")
+  .firestore.document("resep/{resepId}")
+  .onDelete((snapshot) => {
+    return index.deleteObject(snapshot.id).catch((err) => {
+      console.log(err);
+    });
+  });
---
+++ js/SearchPage.js
@@ -0,0 +1,241 @@
+$(document).scroll(function () {
+  $(".navbar").toggleClass(
+    "bg-transparant",
+    $(this).scrollTop() < $(".navbar").height()
+  );
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const db = firebase.firestore();
+  const dropdownWrapper = document.getElementById("dropdownWrapper");
+  const params = new URLSearchParams(window.location.search);
+  const judulResep = params.get("resep");
+  const client = algoliasearch(
+    "VC8WCMFUPU",
+    "71f394aa7d0003076acb0364cbed31c8"
+  );
+  const index = client.initIndex("resep");
+
+  firebase.auth().onAuthStateChanged(async function (user) {
+    let BahanFilter = [];
+
+    function addListener() {
+      $(".btn-TambahCookLater").click((e) => {
+        const idResep = e.target.dataset.id;
+        db.collection("Users")
+          .doc(user.uid)
+          .update({
+            listCookLater: firebase.firestore.FieldValue.arrayUnion(idResep),
+          })
+          .then(() => {
+            console.log("Berhasil disimpan");
+            db.collection("resep")
+              .doc(idResep)
+              .update({
+                cookLaterCount: firebase.firestore.FieldValue.increment(1),
+              })
+              .then(() => {
+                console.log("berhasil");
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+      $(".btn-hapusCookLater").click((e) => {
+        const idResep = e.target.dataset.id;
+        db.collection("Users")
+          .doc(user.uid)
+          .update({
+            listCookLater: firebase.firestore.FieldValue.arrayRemove(idResep),
+          })
+          .then(() => {
+            console.log("Berhasil disimpan");
+            db.collection("resep")
+              .doc(idResep)
+              .update({
+                cookLaterCount: firebase.firestore.FieldValue.increment(-1),
+              })
+              .then(() => {
+                console.log("berhasil");
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    }
+
+    function RenderReseps(data, arrCookLaterList) {
+      let listCard = "";
+      data.forEach((doc) => {
+        let item = doc;
+        let id = doc.objectID;
+        let varButton = "";
+
+        if (!user) {
+          varButton = `
+                <button type="button" class="btn btn-BelumLogin" data-id=${id} data-toggle="modal" data-target="#ModalLogin">
+                    <span class="far fa-bookmark" data-id=${id}></span>
+                </button>
+                `;
+        } else if (arrCookLaterList.includes(id)) {
+          varButton = `
+                <button type="button" class="btn btn-hapusCookLater" data-id=${id}>
+                    <span class="fas fa-bookmark" data-id=${id}></span>
+                </button>
+                `;
+        } else {
+          varButton = `
+                <button type="button" class="btn btn-TambahCookLater" data-id=${id}>
+                    <span class="far fa-bookmark" data-id=${id}></span>
+                </button>
+                `;
+        }
+        const { thumbnail, judul, pembuatResep, bahan } = item;
+        listCard += `
+                <div class="card mb-3 p-4">
+                    <div class="row">
+                        <div class="col-md-4 mb-md-0 mb-3">
+                            <div class="imgThumb">
+                              <a href="./Resep.html?resep=${judul.replace(
+                                " ",
+                                "-"
+                              )}" >
+                                <img src="${thumbnail}" alt="${judul}">
+                              </a>    
+                            </div>
+                        </div>
+                        <div class="col-md-8 text-left">
+                            <a href="./Resep.html?resep=${judul.replace(
+                              " ",
+                              "-"
+                            )}" >
+                              <h3 class="font-weight-bold text-dark">${judul}</h3>
+                            </a> 
+                            <h5>Oleh ${pembuatResep.nama}</h5>
+                            <hr />
+                            <br />
+                            <p><b>Bahan</b> : ${
+                              bahan.length > 0 ? bahan.join(",") : bahan[0]
+                            }</p>
+                            <div class="btnWrapper">    
+                                <a href="./Resep.html?resep=${judul.replace(
+                                  " ",
+                                  "-"
+                                )}" class="btn">
+                                <span class="far fa-comment"></span>
+                                </a>
+                                ${varButton}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+      });
+      document.getElementById("hits").innerHTML = listCard;
+      addListener();
+    }
+
+    function Search(query, arrCookLaterList = []) {
+      index
+        .search(query, {
+          facetFilters: [BahanFilter],
+        })
+        .then((res) => {
+          const { hits } = res;
+          RenderReseps(hits, arrCookLaterList);
+        });
+    }
+
+    if (!user) {
+      dropdownWrapper.innerHTML = `
+            <a class="dropdown-item" href = "./Login.html" > Login</a >
+            <a class="dropdown-item" href="./Register.html">Regis</a>
+          `;
+      await db
+        .collection("Data")
+        .doc("BuatSearch")
+        .get()
+        .then((doc) => {
+          const data = doc.data();
+          var instance = $("#ListBahan").magicSuggest({
+            data: data.listBahan,
+            allowFreeEntries: false,
+          });
+
+          $(instance).on("selectionchange", function (e, m) {
+            BahanFilter = instance.getValue().map((data) => {
+              return `bahan:${data}`;
+            });
+            Search("");
+          });
+        });
+      if (judulResep) {
+        Search(judulResep.replace("+", " "), userDB.listCookLater);
+      } else {
+        Search("");
+      }
+    } else {
+      dropdownWrapper.innerHTML = `
+            <a class="dropdown-item" href = "./dashboard.html" >Dashboard</a >
+            <a class="dropdown-item" href="./update.html">Update Profile</a>
+            <button class="btn dropdown-item" id="logout">
+                Signout
+            </button>
+          `;
+      document.getElementById("logout").addEventListener("click", (e) => {
+        e.preventDefault();
+        return firebase
+          .auth()
+          .signOut()
+          .then(function () {
+            window.location.href = "./Login.html";
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      });
+      db.collection("Users")
+        .doc(user.uid)
+        .onSnapshot(async (snap) => {
+          const userDB = snap.data();
+          await db
+            .collection("Data")
+            .doc("BuatSearch")
+            .get()
+            .then((doc) => {
+              const data = doc.data();
+              let ListBahan = "";
+              var instance = $("#ListBahan").magicSuggest({
+                data: data.listBahan,
+                allowFreeEntries: false,
+              });
+
+              $(instance).on("selectionchange", function (e, m) {
+                BahanFilter = instance.getValue().map((data) => {
+                  return `bahan:${data}`;
+                });
+                Search("", userDB.listCookLater);
+              });
+            });
+          if (judulResep) {
+            Search(judulResep.replace("+", " "), userDB.listCookLater);
+          } else {
+            Search("", userDB.listCookLater);
+          }
+          document
+            .getElementById("searchbox")
+            .addEventListener("change", (e) => {
+              Search(e.target.value, userDB.listCookLater);
+            });
+        });
+    }
+  });
+});
